b79283c6ff1f6660d0ed2c816c8bc3ed1e7b51c1
Fu1Hui <fuyihui1028@gmail.com>
2023-11-29 13:49:24 +0800

    update

--- TextBook/AAAAAA.c
+++ TextBook/AAAAAA.c
@@ -1,35 +1,29 @@
-#include <stdio.h>
-int main()
+#include"stdio.h"
+int trap(int *height, int heightSize)
 {
-    void countAndOut(int *, int);
-    int i, n, num[50];
-    // printf("\ninput number of person: n=");
-    scanf("%d", &n); //输入人数，不超过50
-    for (i = 0; i < n; i++)
-        num[i] = i + 1;
-    countAndOut(num, n);
-    return 0;
+    int lp = 0, rp = 1, sum = 0;
+    if (heightSize == 0)
+        return 0;
+    while (height[lp] == 0)
+    {
+        lp++;
+        rp++;
+    }
+    while (rp < heightSize)
+    {
+        while (height[rp] < height[lp])
+        {
+            sum += height[lp] - height[rp];
+            rp++;
+        }
+        lp = rp;
+        rp = lp + 1;
+    }
+    return sum;
 }
 
-void countAndOut(int array[], int n)
-{
-    int flag = 0, i = 0, j, m = n;
-    while (m - 1)
-    {
-        i = i % n;
-        flag++;
-        if (flag == 3 && array[i] != 0)
-        {
-            flag = 0;
-            array[i] = 0;
-            m--;
-        }
-        i++;
-    }
-    for (j = 0; j < n; j++)
-    {
-        if (array[j] != 0)
-            break;
-    }
-    printf("The last one is NO.%d\n", array[j]);
+int main(void){
+    int height[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, heightSize = 12,s;
+    s = trap(height, heightSize);
+    printf("%d", s);
 }
